9dce34f63219c0e9e1716afda879f877ee49d32c
tandexiang <tandexiang@shein.com>
2019-12-22 17:20:35 +0800

    add fonts

---
+++ scripts/fonts.sh
@@ -0,0 +1,4 @@
+cp $RESOURCES/fonts/FireCode/*.ttf /Library/Fonts/
+cp $RESOURCES/fonts/OperatorMono/*.otf /Library/Fonts/
+cp $RESOURCES/fonts/RebotoMono/*.ttf /Library/Fonts/
+cp $RESOURCES/fonts/SourceCodePro/*.ttf /Library/Fonts/
