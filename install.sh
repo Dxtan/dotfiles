d551a79a9c8dbc8d2463cb173a7cb04de5b3dac0
tandexiang <tandexiang@shein.com>
2019-12-22 18:00:04 +0800

    update

--- install.sh
+++ install.sh
@@ -1,36 +1,5 @@
 #!/bin/sh
-
 echo "Setting up your Mac now..."
-DOTFILES=$HOME/.dotfiles
-RESOURCES=$HOME/.dotfiles/resources
-
-#Git
-if test ! $(which git); then
-  xcode-select --install
-fi
-
-#zsh
-source ./scripts/zsh.sh
-
-# Install global Composer packages
-source ./scripts/composer.sh
-
-# Install Laravel Valet
-# $HOME/.composer/vendor/bin/valet install
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-source ./scripts/envfiles.sh
-
-# Symlink the Mackup config file to the home directory
-source ./scripts/makeup.sh
-
-#Homebrew
-source ./scripts/homebrew.sh
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-# source .macos
+source ./scripts/bootstrap.sh
 
 echo 'Done!';
-
-source ./scripts/open-url.sh
--- scripts/booostrap.sh
+++ scripts/booostrap.sh
@@ -1,3 +1,6 @@
+DOTFILES=$HOME/.dotfiles
+RESOURCES=$HOME/.dotfiles/resources
+
 promptyn () {
     while true; do
         read -p "$1 " yn
@@ -8,3 +11,32 @@ promptyn () {
         esac
     done
 }
+
+#Git
+if test ! $(which git); then
+  xcode-select --install
+fi
+
+#zsh
+source ./scripts/zsh.sh
+
+# Install global Composer packages
+source ./scripts/composer.sh
+
+# Install Laravel Valet
+# $HOME/.composer/vendor/bin/valet install
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+source ./scripts/envfiles.sh
+
+# Symlink the Mackup config file to the home directory
+source ./scripts/makeup.sh
+
+#Homebrew
+source ./scripts/homebrew.sh
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+# source .macos
+
+source ./scripts/open-url.sh
