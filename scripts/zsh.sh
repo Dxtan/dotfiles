d6121267a714eeed84428f4b842f58f614e81a5a
tandexiang <tandexiang@shein.com>
2019-12-22 14:00:38 +0800

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,9 +1,9 @@
 if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 fi
-if [ ! -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]; then
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
-if [ ! -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]; then
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
     git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
