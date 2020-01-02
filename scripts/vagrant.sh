91c1c1f9dc92f1f6ab0e451fa2636be4f200b64f
tandexiang <tandexiang@shein.com>
2020-01-02 10:47:43 +0800

    update install homestead

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,8 +1,4 @@
 if promptyn "Do you want to install homestead (Y/n)?"; then
-    if [ ! -d $HOME/.vagrant.d/boxes/laravel-VAGRANTSLASH-homestead ]; then
-        vagrant box add laravel/homestead
-    fi
-
     if [ ! -d $HOME/Homestead ]; then
         git clone https://github.com/Dxtan/Homestead.git $HOME/Homestead
         cd $HOME/Homestead
@@ -10,6 +6,8 @@ if promptyn "Do you want to install homestead (Y/n)?"; then
         git remote add upstream https://github.com/laravel/homestead.git
         git fetch -p && git merge upstream/release
         chmod 775 $HOME/Homestead/init.sh
+        sh $HOME/Homestead/init.sh
+        vagrant up
         cd $DOTFILES
     fi
 fi
