482239066870af7d3aa3191e232a64827c79873b
tandexiang <tandexiang@shein.com>
2020-01-02 10:47:58 +0800

    composer install friendsofphp/php-cs-fixer

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,6 +1,6 @@
 echo "Installing composer"
 
-if [ ! -f $HOME/.composer/vendor/bin/laravel ]; then
+if [ ! -f $HOME/.composer/vendor/bin/php-cs-fixer ]; then
 	composer config -g repo.packagist composer https://packagist.phpcomposer.com
-    /usr/local/bin/composer global require laravel/installer
+    /usr/local/bin/composer global require friendsofphp/php-cs-fixer
 fi
