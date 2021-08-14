cbc17a0dfca9abf89aa052f4c54bedf9720f2f77
tandexiang <tandexiang@shein.com>
2021-08-14 13:03:11 +0800

    update preferences.sh

--- scripts/preferences.sh
+++ scripts/preferences.sh
@@ -1,8 +1,8 @@
 if promptyn "Do you want to restore Sublime preferences (Y/n)?"; then
-	if [ ! -d $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.cache ]; then
+	if [ ! -d $HOME/Library/Application\ Support/Sublime\ Text//Packages/User/Package\ Control.cache ]; then
 		echo "Installing Sublime Preferences"
 		ln -s  /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
-		rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-		ln -s $HOME/.dotfiles/Preferences/Sublime $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+		rm -rf $HOME/Library/Application\ Support/Sublime\ Text/Packages/User
+		ln -s $HOME/.dotfiles/Preferences/Sublime $HOME/Library/Application\ Support/Sublime\ Text/Packages/User
 	fi
 fi
